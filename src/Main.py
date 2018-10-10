93400402d759aafaa88968d049e1598180d905cd
Zucchi43 <Gabrielzucchi43@gmail.com>
2018-10-10 19:34:48 -0300

    Arrumando

--- src/Main.py
+++ src/Main.py
@@ -9,12 +9,14 @@ sensor1.mode = sensor1.MODE_COL_COLOR #modo detecção de  cor padrao
 def andar_frente():
     #run_forever until sees a color different than white
     #while sensor1.color==6 or sensor1.color==0:
-    rodas.on(60,60)
+    rodas.on(-20,-20)
+    time.sleep(5)
+    quit();
 
 def virar(graus):
 
     if graus == 90:
-        rodas.on_for_seconds(50,-50,1)
+        rodas.on_for_seconds(SpeedPercent(50),-50,1)
     if graus == -90:
         rodas.on_for_seconds(-50,50,1)
     if graus == 180:
@@ -26,5 +28,3 @@ if __name__=="__main__":
     virar(90)
     time.sleep(5)
     andar_frente()
-    time.sleep(5)
-    quit();
