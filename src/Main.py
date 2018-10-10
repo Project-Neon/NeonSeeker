28492027559625f82cd321e4151b939d1ab38c74
Zucchi43 <Gabrielzucchi43@gmail.com>
2018-10-10 18:43:55 -0300

    Arrumando os bang

--- src/Main.py
+++ src/Main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from ev3dev2.sensor import *
 from ev3dev.ev3 import *
 from ev3dev2.sensor.lego import ColorSensor
@@ -5,7 +6,7 @@ from ev3dev2.motor import OUTPUT_A,OUTPUT_B,MoveTank
 rodas=MoveTank(OUTPUT_A,OUTPUT_B)
 sensor1=ColorSensor(INPUT_1)
 sensor1.mode = sensor1.MODE_COL_COLOR #modo detecção de  cor padrao
-def andar_frente:
+def andar_frente():
     #run_forever until sees a color different than white
     while sensor1.color==6 or sensor1.color==0:
         rodas.on(60,60)
@@ -17,7 +18,7 @@ def virar(graus):
     if graus == -90:
         rodas.on(SpeedPercent(-50),SpeedPercent(50),1)
     if graus = 180:
-        rodas,on(SpeedPercent(50),SpeedPercent(-50),2)
+        rodas.on(SpeedPercent(50),SpeedPercent(-50),2)
     else:
         pass
 
