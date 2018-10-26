f76cba5a5c52d8c74532fc60687e521819534aee
Felipe Andrade <41554760+FRAndrade@users.noreply.github.com>
2018-10-26 00:40:07 -0300

    Update Anda.py

--- src/Anda.py
+++ src/Anda.py
@@ -2,34 +2,27 @@
 from ev3dev2.sensor import *
 from ev3dev.ev3 import *
 from time import sleep
+from ev3dev2.motor import OUTPUT_A,OUTPUT_B,MoveTank,SpeedPercent
+sensor1.mode = sensor1.MODE_COL_COLOR
 class Anda:
-	def __init__():
-		mA = LargeMotor(OUTPUT_A)
-		mB = LargeMotor(OUTPUT_B)
-		print("HEAVY ENGINE READY!!")
-	def stop():
-		mA.run_forever(speed_sp=0)
-		mB.run_forever(speed_sp=0)		
-	def vira90(): ## Descobrir qual lado é 90
-		mA.run_forever(speed_sp=-500)
-		mB.run_forever(speed_sp=500)
-		## Descobrir Quanto tempo ele tem que ser mantido
-		sleep(1)
-	def vira-90(): ## Descobrir qual lado é -90
-		mA.run_forever(speed_sp=500)
-		mB.run_forever(speed_sp=-500)
-		## Descobrir Quanto tempo ele tem que ser mantido
-		sleep(1)
-		#stop()
-	def vira180(): ## Descobrir qual lado é -90
-		mA.run_forever(speed_sp=500)
-		mB.run_forever(speed_sp=-500)
-		## Descobrir Quanto tempo ele tem que ser mantido
-		sleep(2)
-		stop()
-	def frente():
-		mA.run_forever(speed_sp=900)
-		mB.run_forever(speed_sp=900)
-	##def alinhamento()
-		## Descobrir como faz o alinhamento
-		
+        def __init__():
+                rodas=MoveTank(OUTPUT_A,OUTPUT_B)
+                print("HEAVY ENGINE READY!!")
+
+        def virar(graus):#fun    o de virada relativa a posi  ao
+                if graus<0:
+                    rodas.on_for_seconds(-50,50,abs(graus)*(0.5/90))
+                elif(graus==0): pass
+                else:
+                    rodas.on_for_seconds(50,-50,abs(graus)*(0.5/90))
+
+        def stop():
+                rodas.on(0,0)
+        def frente():
+                rodas.on(-20,-20)
+
+
+
+
+
+
