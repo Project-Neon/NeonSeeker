7c60abc21c6987ec71790d79ec966d0b4e498e5a
Zucchi43 <Gabrielzucchi43@gmail.com>
2018-10-31 17:59:46 -0300

    Ajustes e alinhamento e deteccao de boneco

--- src/Main.py
+++ src/Main.py
@@ -5,8 +5,8 @@ import time
 print("ev3dev.ev3", end='                      \r')
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent
 print("motores importados", end='                      \r')
-from ev3dev2.sensor.lego import ColorSensor
-from ev3dev2.sensor import INPUT_1, INPUT_2
+from ev3dev2.sensor.lego import ColorSensor,UltrasonicSensor
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3
 print("Sensores importados", end='                      \r')
 from threading import Thread
 from math import sqrt
@@ -23,31 +23,65 @@ cor_atual=""
 tentativa=0
 start_time=0
 c=""
+mochila=0
 cores = pickle.load(open("Cores.p", "rb"))
 Sensor_direita = ColorSensor(INPUT_2)
 Sensor_esquerda = ColorSensor(INPUT_1)
 Sensor_direita.mode = Sensor_direita.MODE_RGB_RAW
 Sensor_esquerda.mode = Sensor_esquerda.MODE_RGB_RAW
+Sensor_sonic = UltrasonicSensor(INPUT_3)
+Sensor_sonic.mode=Sensor_sonic.MODE_US_DIST_CM
 print("Declarando tudo!", end='                      \r')
 #FUNÇÔES DE LOCOMOÇÂO
 def retorno(t):#função para o retorno
     global tentativa,start_time,c
     #print ('viu preto')
     while c=='Black' or c=='White':rodas.on(20,20)
-    time.sleep(1)
+    time.sleep(0.8)#tempo para a parada no meio do quadrado
     rodas.off()
     #rodas.on_for_seconds(20,20,t)#volta até o ultimo ponto de referencia
     tentativa+=1#indica que foi feita uma tentativa que falhou
     cor=c
     procurar_proximo()#vira conforme as orientações que são possiveis
     start_time = time.time()#reseta o timer
-    sair_da_cor_atual()#anda um pouco a frente para nao o robo não reconhecer o mesmo ponto de referencia como um novo ponto
+    alinha(0.02,230,30)#anda um pouco a frente para nao o robo não reconhecer o mesmo ponto de referencia como um novo ponto
 
 def sair_da_cor_atual():#TROCAR PELO ALINHAMENTO
     global c
     while c!='White':
         rodas.on(-20,-20)
     rodas.off()
+
+def alinha(Kp,target,margem):
+    global e,d
+    erroE=1
+    erroD=1
+    while(erroE != 0 or erroD != 0) :
+        atualD = d[0]+d[1]+d[2]
+        erroD=atualD - target
+        if abs(erroD)<margem:
+            erroD=0
+        outputD = erroD* Kp
+        atualE = e[0]+e[1]+e[2]
+        erroE=atualE - target
+        if abs(erroE)<margem:
+            erroE=0
+        outputE = erroE* Kp
+        if outputE>40:
+            outputE = 40
+        elif outputE<-40:
+            outputE=-40
+        if outputD>40:
+            outputD = 40
+        if erroE == 0 and erroD == 0:
+            rodas.off()
+        else:
+                rodas.on(outputE,outputD)
+    while c!='White':
+        rodas.on(-20,-20)
+        rodas.off()
+
+
 
 def andar_frente():#Corrigir todos os tempos presentes aqui a fim de utilizar com o robo e pista finais
     global cor_atual,start_time,quads,c
@@ -58,9 +92,7 @@ def andar_frente():#Corrigir todos os tempos presentes aqui a fim de utilizar co
             return (time.time()-start_time)#ACERTAR ESTE CALCULO PARA A CONTAGEM COMEÇAR DO MEIO DO QUADRADO COLORIDO
         elif c!="White" and c!="Black" and start_time != 0:
             if(Confirmar_cor(c)):
-                # rodas.on_for_seconds(-20,-20,2.5)
-                # print ("Achou alguma cor: ",c)
-                verificar_plaza()#trocar por função de indentificação do plaza
+                verificar_plaza()
                 rodas.off()
                 return (time.time()-start_time)#se achar branco/cor de indicação retorna o tempo entre os pontos e para de andar
         elif c!="White" and c!="Black" and start_time==0:
@@ -69,13 +101,12 @@ def andar_frente():#Corrigir todos os tempos presentes aqui a fim de utilizar co
                 time.sleep(1.42)
                 procurar_proximo()#vira se ver branco, começa o timer e continua a andar para frente
                 start_time = time.time()
-                sair_da_cor_atual()
+                alinha(0.02,230,30)
                 #alinhar (lembrar de descontar o tempo de alinhamento na variavel start_time)
         while c=='White':
-            #while não ver o boneco
-            rodas.on(-20,-20)
+            #Anda pelo branco em procura do boneco se a mochila nao esta carregada(mochila==0).Senão apenas anda para frente no branco
+            procurar_passageiro()
 def virar(graus):#função de virada relativa a posiçao
-        global orientacao
         if graus<0:
             rodas.on_for_seconds(-50,50,abs(graus)*(0.40/90))
         elif(graus==0): pass
@@ -135,10 +166,11 @@ def diferente_de(*cor):
     else: return 0
 
 def cor_th():
-    global c
+    global c,e,d
     while(1):
         c=cor_mais_proxima(Sensor_direita.rgb)
-
+        d=Sensor_direita.rgb
+        e=Sensor_esquerda.rgb
 def Confirmar_cor(cor_vista):
     global c
     time.sleep(0.08)
@@ -152,7 +184,7 @@ def verificar_plaza():
     if c!='Black':
         mudanca = 0
         cor_momento = c
-        goiaba = Thread(target=rodas.on_for_seconds, args=(-20, -20, 1.35,))
+        goiaba = Thread(target=rodas.on_for_seconds, args=(-20, -20, 1.27,))
         goiaba.start()
         while(goiaba.is_alive()):
             if (cor_momento != c):
@@ -164,10 +196,26 @@ def verificar_plaza():
             # print(" NAO PLAZA")
         goiaba.join()
     rodas.off()
+
+
 #FIM DAS FUNÇÕES DO PLAZA
 
 #FUNÇÕES DA MOCHILA(EQUIPAMENTO DE CAPTURAR BONECO)
-
+def procurar_passageiro():
+    global mochila,c
+    while c == 'White':
+        rodas.on(-20,-20)
+        if Sensor_sonic.distance_centimeters<25 and mochila==0:
+            time.sleep(0.3)#regular para o robo parar com seu centro de giro alinhado com o boneco detectado
+            rodas.off()
+            dist=Sensor_sonic.distance_centimeters
+            virar(90)
+            rodas.on_for_seconds(-20,-20,dist*0.04)#regular o valor de forma ao robo pegar o boneco
+            time.sleep(1)
+            mochila=1
+            rodas.on_for_seconds(20,20,dist*0.055)
+            virar(-90)
+            rodas.off()
 #FIM DAS FUNÇÕES DE MOCHILA
 
 #FUNÇÕES DE INFORMAÇÃO
