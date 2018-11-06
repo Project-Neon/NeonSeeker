5d1d2cdf33090aaddae8e0ed0cf9898c426ced81
Felipe Andrade <41554760+FRAndrade@users.noreply.github.com>
2018-11-06 00:37:44 -0200

    Update Mochila.py

--- src/Modulos/Mochila.py
+++ src/Modulos/Mochila.py
@@ -9,3 +9,7 @@ def Mochila_pega():
     Mochila.on_for_rotations(SpeedPercent(-20), 0.15)
 def Mochila_sobe():
     Mochila.on_for_rotations(SpeedPercent(-20), 0.53)
+def set_mochila(cond): ## Define se temos um boneco ou não
+	self.cond=cond
+def get_mochila():     ## Caso True ele entra no plaza    
+	return cond        ## Caso False ele não entra no plaza
